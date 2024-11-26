93e5e95a004eba70114f4ffa6499e6a61cf8f4cb
CJ <lim@chernjie.com>
2024-11-26 01:08:47 -0800

    refactor readInput to handle less logic, and pass logic back to caller

--- src/utils/readInput.js
+++ src/utils/readInput.js
@@ -1,31 +1,33 @@
 #!/usr/bin/env node
 
-import { readFileSync } from 'fs'
+import { readFile } from 'fs/promises'
 
 async function sampleUsage() {
   const { args, options } = parseArgs(process.argv.slice(2))
-  const input = await readInput(
-    { args },
-    argv => readFiles(argv).shift(),
-    data => data,
-  )
+
+  if (!args.length && !options.stdin) {
+    throw new Error('No input provided')
+  }
+
+  // arguments takes precedence over stdin
+  const input = args.length
+    ? await Promise.all(readFiles(args))
+    : await options.stdin
+
   printJSON(input)
 }
 
-export function readInput({ args }, argvCallback, stdinCallback) {
+export function readInput() {
   return new Promise((resolve, reject) => {
-    if (args.length) {
-      return resolve(argvCallback(args))
-    }
     const buffer = []
     process.stdin.setEncoding('utf8')
     process.stdin.on('data', data => buffer.push(data))
-    process.stdin.on('end', () => resolve(stdinCallback(buffer.join(''))))
+    process.stdin.on('end', () => resolve(buffer.join('')))
   })
 }
 
 export function parseArgs(argv) {
-  return argv.reduce((last, arg)=> {
+  const { args, options } = argv.reduce((last, arg)=> {
       if (/^--/.test(arg)) {
         const equals = arg.split('=')
         last.options[equals.shift().replace(/^--/, '')] = equals.join('=') || true
@@ -35,10 +37,19 @@ export function parseArgs(argv) {
       return last
     }, { args: [], options: {} }
   )
+  if (!args.length) {
+    options.stdin = readInput()
+  }
+  return { args, options }
 }
 
 export function readFiles(files) {
-  return files.map(file => readFileSync(file).toString())
+  return files
+    .map(file => path.resolve(process.cwd(), file))
+    .map(file =>
+      readFile(file)
+        .then(res => res.toString())
+    )
 }
 
 export function printJSON(data) {
